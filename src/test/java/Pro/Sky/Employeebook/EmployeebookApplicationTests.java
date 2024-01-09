5bcbdfba1d2ebce453d03a586c14b745981ca060
IMBor1 <ilacerber1@gmail.com>
2024-01-09 14:37:47 +0300

    init

---
+++ src/test/java/Pro/Sky/Employeebook/EmployeebookApplicationTests.java
@@ -0,0 +1,13 @@
+package Pro.Sky.Employeebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
